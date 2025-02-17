2ca44be5942ad566ea91e8c63632179e801408d2
RifaSavvy <149214453+RifaSavvy@users.noreply.github.com>
2025-02-17 11:21:05 +0530

    Add files via upload

---
+++ LAB 4/srtf1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+void main(){
+	int n,at[10],bt[10],ct[10],wt[10],temp[10],tat[10], p[10],smallest,count = 0,time,end = 0;
+	double avg_wt = 0, avg_tt = 0;
+	printf("Enter the number of processess");
+	scanf("%d",&n);
+	printf("Enter AT and BT for each process:\n");
+	for (int i=0;i<n;i++) {
+        	printf("P%d: ",i+1);
+        	scanf("%d %d",&at[i],&bt[i]);
+        	temp[i]=bt[i]; 
+        	p[i]=i+1;
+        	}
+        bt[9]=9999;
+        for (time=0;count!=n;time++) {
+        smallest=9;
+        for(int i=0;i<n;i++) {
+            if(at[i]<=time && bt[i]<bt[smallest] && bt[i]>0) {
+                smallest=i;
+            }}
+        bt[smallest]--;
+        if (bt[smallest]==0) {
+            count++;
+            end =time+1;
+            ct[smallest]=end;
+            tat[smallest] =ct[smallest]-at[smallest];  
+            wt[smallest] = tat[smallest]-temp[smallest];
+        }}
+	printf("\n-------------------------------------------------------------------------\n");
+        printf("Process\tAT\tBT\tCT\tTAT\tWT\n");
+        printf("---------------------------------------------------------------------------\n");
+        for (int i=0;i<n;i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",p[i],at[i],temp[i],ct[i],tat[i],wt[i]);
+        avg_tt+=tat[i];
+        avg_wt+=wt[i];
+        }
+ 	printf("\n-------------------------------------------------------------------------\n");
+        printf("\nAverage Turnaround Time: %.2f\n",avg_tt/n);
+        printf("Average Waiting Time: %.2f\n",avg_wt/n);
+        }
